bfd63c10965c31da6e705a588135eb5e830ce1c4
Chris A. Upshaw <chrisaupshaw@gmail.com>
2023-09-17 23:08:07 -0700

    WIP

--- Cheap.lean
+++ Cheap.lean
@@ -1,1 +1,28 @@
-def hello := "world"
+import Mathlib
+
+namespace NSA
+
+class Filter (U : Type → Type) extends Applicative U where
+  filter : U Prop → Prop
+
+open Filter
+
+infix:60 "∧'" => λ p q => (· ∧ ·) <$> p <*> q
+infix:60 "∨'" => λ p q => (· ∨ ·) <$> p <*> q
+def not' {U : Type → Type} [Filter U] (p : U Prop) : U Prop := (¬ ·) <$> p
+
+class LawfulFilter (U : Type → Type) extends Filter U where
+  to_LawfulApplicative : LawfulApplicative U
+  filter_pure : ∀ (p : Prop), filter (pure p : U Prop) = p
+  filter_intersection : ∀ (p q : U Prop), filter (p ∧' q) = filter p ∧ filter q
+  filter_all : (s : X → Prop) → (∀ (x : X), s x) → ∀ u : U X, filter (s <$> u)
+
+class UltraFilter (U : Type → Type ) extends Filter U where
+  ultra : ∀ p, filter p ∨ filter (not' p)
+
+class LawfulUltraFilter (U : Type → Type) extends LawfulFilter U, UltraFilter U where
+ -- empty
+
+class NSA (U : Type → Type) extends Filter U where
+  idealization : ∀ (r : X → Y → Prop), 
+    (∀ s : Finset X, ∃ y, ∀ x ∈ s, filter (r x <$> y)) ↔ ∃ y, ∀ x, filter (r <$> pure x <*> y) 
--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,13 @@
 import Lake
 open Lake DSL
 
-package «cheap» {
-  -- add any package configuration options here
+package cheap {
+  -- add package configuration options here
 }
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «Cheap» {
-  -- add any library configuration options here
+lean_lib CHEAP {
+  -- add library configuration options here
 }
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4"@"3045aef4ffb0fd709de915066088b9bb54ba54a6"
